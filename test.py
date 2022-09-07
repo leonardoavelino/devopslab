b430410f64ee0da3b180b77b15ce3a0603e0ea92
leonardoavelino <leonardoavelino@gmail.com>
2022-09-07 00:46:42 -0300

    Modifying Application text on main page

--- test.py
+++ test.py
@@ -17,4 +17,4 @@ class Test(unittest.TestCase):
                                                                                                                                                                                                          
     def test_conteudo(self):                                                                                                                                                                             
         # verifica o retorno do conteudo da pagina                                                                                                                                                       
-        self.assertEqual(self.result.data.decode('utf-8'), "Entrega do Solution Sprint - Fase 05 - Grupo 18")     
+        self.assertEqual(self.result.data.decode('utf-8'), "Entrega da Solution Sprint - Fase 05 - Grupo 18")
